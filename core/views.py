4526ae965522a33406b17c546a510aee73cb8371
João Victor Corrêa <marcus.metri@gmail.com>
2025-10-02 20:40:31 -0300

    initial command

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import os
+
+# Create your views here.
+def dolar_agora(request, ip):
+    return render(request, 'core/dolar_agora.html')
+
+def home(request):
+    return render(request, 'core/index.html')
